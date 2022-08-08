0f4be8853a10e42c36e80f8fc8f190daa27ed72e
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-08 11:44:04 -0700

    first commit

---
+++ OperatorProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Excercise 1
+            var a = 17;
+            int b = 4;
+            int addition = a + b;
+            var quotient = a/b;
+            int remainder = a % b;
+            int subtraction = a - b;
+            if (a == 17 && b == 4) ;
+            Console.WriteLine($"{ a}/{ b} is { quotient} with remainder {remainder}");  
+        }
+    }
+}
