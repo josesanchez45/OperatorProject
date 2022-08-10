9a5ed3340064970a12c1997019a66900da52e3e2
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-10 07:26:26 -0700

    made easier to read

--- OperatorProject/Program.cs
+++ OperatorProject/Program.cs
@@ -14,10 +14,12 @@ namespace OperatorProject
             //Excercise 1
             var a = 17;
             int b = 4;
+
             int addition = a + b;
             var quotient = a / b;
             int remainder = a % b;
             int subtraction = a - b;
+
             if (a == 17 && b == 4) ;
             Console.WriteLine($"{a}/{b} is {quotient} with remainder {remainder}");
             Console.WriteLine();
